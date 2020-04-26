a88d1abbf01415b79768521a28b18b1920ce5692
Adam Wathan <adam.wathan@gmail.com>
2020-04-26 15:10:12 -0400

    Initial support for IE11 target

--- src/plugins/placeholderOpacity.js
+++ src/plugins/placeholderOpacity.js
@@ -1,7 +1,13 @@
 import createUtilityPlugin from '../util/createUtilityPlugin'
 
 export default function() {
-  return createUtilityPlugin('placeholderOpacity', [
-    ['placeholder-opacity', ['--placeholder-opacity']],
-  ])
+  return function({ config, ...args }) {
+    if (config('target') === 'ie11') {
+      return
+    }
+
+    createUtilityPlugin('placeholderOpacity', [
+      ['placeholder-opacity', ['--placeholder-opacity']],
+    ])({ config, ...args })
+  }
 }
